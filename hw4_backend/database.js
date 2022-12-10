c28e6733bda972694b3f41d1324f1f5c4df80442
anett.parismaa <anett.parismaa@ctftech.com>
2022-12-10 21:19:04 +0200

    trying to connect database

--- hw4_backend/database.js
+++ hw4_backend/database.js
@@ -2,10 +2,34 @@
  const Pool = require('pg').Pool;
  const pool = new Pool({
      user: "postgres",
-     password: "[wad22]",
-     database: "[hw4]",
+     password: "wad22",
+     database: "hw4",
      host: "localhost",
      port: "5432"
  });
+
+ const execute = async(query) => {
+    try {
+        await pool.connect(); // create a connection
+        await pool.query(query); // executes a query
+        return true;
+    } catch (error) {
+        console.error(error.stack);
+        return false;
+    }
+};
+
+const createTblQuery = `
+    CREATE TABLE IF NOT EXISTS "users" (
+        id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+        email VARCHAR(200) NOT NULL UNIQUE,
+        password VARCHAR(200) NOT NULL 
+    );`;
+
+execute(createTblQuery).then(result => {
+    if (result) {
+        console.log('Table "users" is created');
+    }
+});
  
  module.exports = pool;
